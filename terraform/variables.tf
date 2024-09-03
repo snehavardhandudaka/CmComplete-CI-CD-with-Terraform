8e820a0741366ba8e398d777eafe3a6f36c2664d
snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>
2024-09-02 22:32:17 -0500

    Delete terraform/variables.tf
    Signed-off-by: snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key_id" {
-  description = "The AWS access key ID"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "The AWS secret access key"
-  type        = string
-}
