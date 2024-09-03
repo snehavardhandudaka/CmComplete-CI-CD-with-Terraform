b9975d98f28d2d2cde2855238952c706ff99bf27
snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>
2024-09-02 22:00:25 -0500

    Create variables.tf
    Signed-off-by: snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key_id" {
+  description = "The AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS secret access key"
+  type        = string
+}
