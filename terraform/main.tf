ac23ff0f4362e2147ae2cc5f680d182649f69b7c
snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>
2024-09-02 22:10:20 -0500

    Update main.tf
    Signed-off-by: snehavardhandudaka <134006008+snehavardhandudaka@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,19 @@
-# Define the AWS region variable
 variable "aws_region" {
   description = "The AWS region to deploy the resources"
   default     = "us-east-2"
 }
 
-# Define the AMI ID variable
 variable "ami_id" {
   description = "The AMI ID to use for the instance"
-  default     = "ami-0c55b159cbfafe1f0"  # Example AMI, change as needed
+  default     = "ami-0c55b159cbfafe1f0"
 }
 
-# Define the instance type variable
 variable "instance_type" {
   description = "The instance type to use for the EC2 instance"
   default     = "t2.micro"
 }
 
-# Define the key name variable
 variable "key_name" {
   description = "The SSH key pair name to use for the instance"
-  default     = "TWN.pub"  # Replace with your SSH key name
+  default     = "TWN.pub"
 }
